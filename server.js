c7b8e1fb0791057b55655a755ea6a8b5352e1e69
Sagar <sagarwalke.web@gmail.com>
2021-05-19 13:27:31 +0530

    Dir updated

--- server.js
+++ server.js
@@ -12,9 +12,18 @@ const server = http.createServer(app);
 //configure server port number
 server.listen(process.env.PORT || 5000);
 
+// step 3: Heroku
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 server.on("listening", () => {
   console.log(`server running at http://localhost:/5000`);
- 
+
   console.log("sagar test msg");
   console.log("checking");
 });

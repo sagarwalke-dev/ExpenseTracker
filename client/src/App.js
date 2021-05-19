36b82d9311ca3dda0ef4793e6ab54ac353f03487
amit15091996 <amitdewangan199@gmail.com>
2021-05-19 09:18:33 +0530

    added footer

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css';
+import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <>
+            <footer class="mainfooter">
+                <div class="footer-middle">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 copy">
+                                <p class="text-center">&copy; Copyright 2021 - Expense Tracker.  All rights reserved.</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+            </footer>
+
+            {/* <Form>
+
+                    <div class="copyright text-center">
+                        Copyright &copy; 2021 <span>Expense Tracker</span>
+                    </div>
+                    
+                </Form> */}
+
+
+        </>
+    );
+}
+
+export default Footer;

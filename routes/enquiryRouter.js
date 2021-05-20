38f46ad09b33e3fabab5edf27efdcebeb879de64
Sagar <sagarwalke.web@gmail.com>
2021-05-20 10:09:51 +0530

    Mail service added for enquiry module

--- routes/enquiryRouter.js
+++ routes/enquiryRouter.js
@@ -4,26 +4,20 @@ const router = express.Router();
 const crud = require("../controller/enquiryController");
 
 //EndPoint for adding data
-router.post("/submitContact",function (req,res,next) {
-  const {uName,uNumber ,uMailId, uDescription} = req.body;
-  if(!uName || !uNumber || !uMailId || !uDescription)
-  {
-      res.status(422).json({ error: `Kindly fill all the details`});
-
+router.post("/submitContact", function (req, res, next) {
+  const { uName, uNumber, uMailId, uDescription } = req.body;
+  if (!uName || !uNumber || !uMailId || !uDescription) {
+    res.status(422).json({ error: `Kindly fill all the details` });
+  } else {
+    let status = crud.addContactData(uName, uNumber, uMailId, uDescription);
+    status
+      .then((result) => {
+        res.status(200).json({ success: `${result}` });
+      })
+      .catch((result) => {
+        res.status(204).json({ error: `${result}` });
+      });
   }
-  else
-  {
-
-  
-   let status = crud.addContactData(uName,uNumber,uMailId,uDescription);
-  status.then(result =>{
-      res.status(200).json({sucess:`${result}`});
-  }).catch(result =>{
-      res.status(204).json({error:`${result}`});
-  })
-}
-
-  
 });
 
 module.exports = router;

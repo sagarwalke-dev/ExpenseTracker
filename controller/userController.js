78aa084a57778a9b17d7362d28bcf5e8cdcfbb1c
Sagar <sagarwalke.web@gmail.com>
2021-05-21 16:29:31 +0530

    Code clean

--- controller/userController.js
+++ controller/userController.js
@@ -1,74 +1,59 @@
 //load modules.
-const User=require('../model/userSchema');
-const db=require('../services/database');
-const bcrypt=require('bcrypt');
-
+const User = require("../model/userSchema");
+const db = require("../services/database");
+const bcrypt = require("bcrypt");
 
 let userRegistration = async (req, res, next) => {
-    const { name, email, contact, work, password, gender } = req.body;
- 
-   try {
-     if (!name || !email || !contact || !work || !password || !gender) {
-       return res.status(422).json({ error: "Please fill all data" });
-     }
-     const userExist = await User.findOne({ userEmail: email });
-     if (userExist) {
-       return res.status(422).json({ error: "Email already exist" });
-     }
-  //PASSWORD ENCRPTING
-  const passwordHash=await bcrypt.hash(password,10);
-  console.log(passwordHash)
-     const user = new User({
-       userName: name,
-       userEmail: email,
-       userContact: contact,
-       userWork: work,
-       userPassword: passwordHash,
-       userGender: gender,
-     });
- 
-     const response = await user.save();
-     if (response) {
-       return res.status(201).json({ message: "User registered successfully" });
-     } else {
-       return res.status(500).json({ error: "User registration failed" });
-     }
-   } catch (err) {
-     res.status(500).json({ error: "Something went wrong" });
-     console.log(err);
-   }
- };
+  const { name, email, contact, work, password, gender } = req.body;
+
+  try {
+    if (!name || !email || !contact || !work || !password || !gender) {
+      return res.status(422).json({ error: "Please fill all data" });
+    }
+    const userExist = await User.findOne({ userEmail: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exist" });
+    }
+    //PASSWORD ENCRPTING
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+    const user = new User({
+      userName: name,
+      userEmail: email,
+      userContact: contact,
+      userWork: work,
+      userPassword: passwordHash,
+      userGender: gender,
+    });
+
+    const response = await user.save();
+    if (response) {
+      return res.status(201).json({ message: "User registered successfully" });
+    } else {
+      return res.status(500).json({ error: "User registration failed" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Something went wrong" });
+    console.log(err);
+  }
+};
 
 //Adding Expenses
 let addExpenses = async (req, res, next) => {
-  const { userEmail, itemName,category,amount ,expenseDate} = req.body;
-  console.log(userEmail, itemName,category,amount,expenseDate);
-  
+  const { userEmail, itemName, category, amount, expenseDate } = req.body;
+  console.log(userEmail, itemName, category, amount, expenseDate);
+
   //Adding based on perticuler user
   let validUser = await User.findOne({ userEmail: userEmail });
-  if(validUser)
-{
-      let expense = await validUser.generateAuthToken(itemName,category,amount,expenseDate);
-      return res.status(200).json({ message: "Added successfully" });
-    } 
+  if (validUser) {
+    let expense = await validUser.generateAuthToken(
+      itemName,
+      category,
+      amount,
+      expenseDate
+    );
+    return res.status(200).json({ message: "Added successfully" });
+  }
 };
 
-
-
-
-//creating method for adding Contact Data
-let userContact = async (name,number,mailId,description)=>{
-    try{
-    let status = await  schema({userName:name,userNumber:number,userMailId:mailId,userDescription:description}).save();
-    return `Your response has submitted successfully`;
-    }
-    catch(error){
-        return `your respose not submitted successfully ${error}`;
-
-
-    }
-
-}
-
-
-module.exports={userRegistration,addExpenses,userContact}
+module.exports = { userRegistration, addExpenses };
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,41 +1,26 @@
 //load modules
-var express = require('express');
-const app=express();
+var express = require("express");
+const app = express();
 var router = express.Router();
-const userController=require('../controller/userController');
+const userController = require("../controller/userController");
 
 //Home page route.
-router.get('/', function (req, res) {
-    console.log("inside home")  
-    
+router.get("/", function (req, res) {
+  console.log("inside home");
 });
 
 //registration route
 router.post("/registration", function (req, res, next) {
-    console.log("start registering  new user...");
-    userController.userRegistration(req, res, next);
-    console.log("end registering  new user...");
-  });
-
-  //Adding Expenses
-  router.post("/addingExpenses",function(req,res,next)
-  {
-    console.log("Adding Expenses...");
-    userController.addExpenses(req,res,next)
-    console.log("Adding Complited...");
-  })
-
-//EndPoint for adding data
-router.get("/addContactData",function (req,res,next) {
-     let status = crud.userContact(req.query.uName,req.query.uNumber,req.query.uMailId,req.query.uDescription);
-    status.then(result =>{
-        res.status(201).json({sucess:`${result}`});
-    }).catch(result =>{
-        res.status(204).json({Error:`${result}`});
-    })
-
-    
+  console.log("start registering  new user...");
+  userController.userRegistration(req, res, next);
+  console.log("end registering  new user...");
 });
 
+//Adding Expenses
+router.post("/addingExpenses", function (req, res, next) {
+  console.log("Adding Expenses...");
+  userController.addExpenses(req, res, next);
+  console.log("Adding Complited...");
+});
 
 module.exports = router;

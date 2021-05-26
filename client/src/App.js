f99a7b6d1e6e7b3ac18ac80c61de84ead490ac0c
amit15091996 <amitdewangan199@gmail.com>
2021-05-26 11:21:46 +0530

    Minor Fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import React from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
-=======
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer";
@@ -12,7 +10,6 @@ import Contact from "./components/Contact";
 import Login from "./components/Login";
 import Home from "./components/Home";
 import "./App.css";
->>>>>>> 78aa084a57778a9b17d7362d28bcf5e8cdcfbb1c
 function App() {
   
   return (
